8f6d141343107057b7040df9ede71f0b86d099c8
sora0920 <sora02230112@gmail.com>
2018-02-15 14:55:29 +0900

    エラー処理を追加

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,30 @@ use std::env;
 use std::io::{BufReader, Read};
 use reqwest::header::{Authorization, Bearer};
 use std::collections::HashMap;
+use reqwest::StatusCode;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let path = &args[1]; // 設定ファイルのPathを入れる(host, token, status, [cw], [vis])
+    let paths: Vec<String> = env::args().skip(1).collect();
+    let mut json: String = String::new();
+    
+    for path in paths {
+        read_file(&path, &mut json);
+        don_post(&json);
+        json = "".to_string();
+    }
+} 
+
+fn read_file(path: &str, mut json: &mut String){
     let mut f = BufReader::new(fs::File::open(path).unwrap());
-    let mut json = String::new();
 
     match f.read_to_string(&mut json){
         Err(why) => panic!("couldn't read {}: {}", path,
                                                    Error::description(&why)),
         Ok(_) => println!("FileReadOK!")
     };
+}
 
+fn don_post(json: &str) {
     let parse_json = json_flex::decode(json.to_owned());
 
     let status: String = parse_json["status"].into_string().unwrap().to_string();
@@ -36,10 +47,17 @@ fn main() {
 
     host = format!("https://{}/api/v1/statuses", &host).to_string();
 
-    let _res = client.post(&host)
+    let resp = client.post(&host)
         .json(&map)
         .header(Authorization(Bearer { token: token }))
-        .send();
-    println!("{} is done", parse_json["name"].into_string().unwrap().to_string());
-} 
+        .send()
+        .unwrap();
 
+    match resp.status() {
+        StatusCode::Ok => println!("{} is done", parse_json["name"].into_string().unwrap().to_string()),
+        StatusCode::PayloadTooLarge => {
+            println!("{}: Request payload is too large!", parse_json["name"].into_string().unwrap().to_string());
+        }
+        s => println!("{}: Received response status: {:?}", parse_json["name"].into_string().unwrap().to_string(), s),
+    };
+}
